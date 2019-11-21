58846f2ad22d26f2b1a41d402173d68aa92ea12c
EDDxample <EDDxample@gmail.com>
2019-11-21 19:20:18 +0100

    add ActionController

--- src/main/java/eddxample/twitch/ActionController.java
+++ src/main/java/eddxample/twitch/ActionController.java
@@ -1,5 +1,12 @@
 package eddxample.twitch;
 
+import java.util.concurrent.atomic.AtomicReference;
+
 public class ActionController {
-    public static String action;
+    public static AtomicReference<String> action;
+
+    void setAction(String s) {
+        action.set(s);
+    }
+
 }
